3b40ca0262d6bf7c3f16365af88e7b67d70f64a4
jocelinkisenga <kisengajocelin7@gmail.com>
2022-05-29 21:32:16 +0200

    ajout des routes

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+
+const cors = require('cors');
 const app = express();
+var CorsOptions = {
+	origin: "http://localhost:8080"
+};
 
-const port = 8000;
+app.use(cors(CorsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req, res)=>{
-	res.set('Content-Type', 'text/html');
-	res.send('hello world');
+app.get('/', (req, res)=>{
+	res.json({name: 'jocelin kisenga'});
 });
 
-app.listen(port, ()=>{
-	console.log('this server is listening to port' +port);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+	console.log(`server running at port ${PORT}`);
 });
